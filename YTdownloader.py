c2a8b78a934939d17339a9fe0f001f54c0c6f0e3
Daniel Gregorio <daniel1wnl@Daniels-MacBook-Pro.local>
2023-10-31 17:49:25 -0700

    created a simple UI that takes user input

--- YTdownloader.py
+++ YTdownloader.py
@@ -8,31 +8,44 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+
+def test():
+    a = customtkinter.CTkLabel(app, text="HELLO APP")
+    a.pack(padx=10,pady=10)
+    print("hello world")
+
 # Settings for the app
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
 app = customtkinter.CTk()
 app.geometry("600x400")
 app.title("YTdownloader")
+title = customtkinter.CTkLabel(app, text="Enter YouTube Link")
+title.pack(padx=10,pady=10)
+videoURL = tkinter.StringVar()
+url = customtkinter.CTkEntry(app, width=300,height=40,textvariable=videoURL)
+url.pack()
+
+download = customtkinter.CTkButton(app, text="Download", command=test)
+download.pack()
+
 app.mainloop()
 
 # Downloading Logic (will change once UI is compleate)
-while True:
-    toDownload = str(input("Enter the YouTube link\n➜ "))
-    print("Would you like to download MP3 or MP4?")
-    print("Enter [1] for MP3 (Audio Only)")
-    print("Enter [2] for MP4 (Video + Audio)")
-    userInput = int(input("➜ "))
-    fromYT = YouTube(url=toDownload)
-    if userInput == 1:
-        audio = fromYT.streams.get_audio_only()
-        outaudio = audio.download(output_path="YTD_Audios")
-        base, ext = os.path.splitext(outaudio)
-        new_file = base + ".mp3"
-        os.rename(outaudio, new_file)
-    if userInput == 2:
-        video = fromYT.streams.get_highest_resolution()
-        video.download(output_path='YTD_videos')
-    end = str(input("Would you like to download again? (Y/n)"))
-    if end in ['n','N']:
-        break
+# while True:
+#     toDownload = str(input("Enter the YouTube link\n➜ "))
+#     print("Would you like to download MP3 or MP4?")
+#     print("Enter [1] for MP3 (Audio Only)")
+#     print("Enter [2] for MP4 (Video + Audio)")
+#     userInput = int(input("➜ "))
+#     fromYT = YouTube(url=toDownload)
+#     if userInput == 1:
+#         audio = fromYT.streams.get_audio_only()
+#         outaudio = audio.download(output_path="YTD_Audios")
+#         base, ext = os.path.splitext(outaudio)
+#         new_file = base + ".mp3"
+#         os.rename(outaudio, new_file)
+#     if userInput == 2:
+#         video = fromYT.streams.get_highest_resolution()
+#         video.download(output_path='YTD_videos')
+#     end = str(input("Would you like to download again? (Y/n)"))
+#     if end in ['n','N']:
+#         break
